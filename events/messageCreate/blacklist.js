45639828a4b38b61593b0462c6b141888ccb3968
Shubham Kumar Pandey <74237720+SHUBHAM1234A@users.noreply.github.com>
2023-05-25 11:35:59 +0530

    Update blacklist.js

--- events/messageCreate/blacklist.js
+++ events/messageCreate/blacklist.js
@@ -1,11 +1,4 @@
-const blackListed = ["bad1", "bad2", "bad3"];
-
 module.exports = (message, client, handler) => {
-  for (let i of blackListed) {
-    if (message.content.includes(i)) {
-      message.delete();
-      message.channel.send("blacklisted!");
-    }
-  }
+  // Nothing here
 };
 
